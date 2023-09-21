3ff47b507c1f9aa02886b05b06a38b60a94ca09b
Mounir Khanouri <m.khanouri@icloud.com>
2023-09-21 15:55:55 +0200

    feat(db): mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot('mongodb+srv://mkhanouri555:RZHE2xJWCxBVjbpJ@cluster0.j8yerwy.mongodb.net/?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
